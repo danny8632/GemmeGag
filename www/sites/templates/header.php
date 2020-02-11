e10c7bf704df695c2b24030b9281f661f4c7c6ff
felix <felix@hipcat.dk>
2020-02-11 13:18:55 +0100

    added link on logo

--- www/sites/templates/header.php
+++ www/sites/templates/header.php
@@ -24,7 +24,7 @@
     <body>
         <div class="header">
             <div class="logo">
-                <a class="logotext" href="" >GemGag</a>
+                <a class="logotext" href="/home" >GemGag</a>
             </div>
             <div class="search">
                 <form>
