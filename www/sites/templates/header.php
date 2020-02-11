23a8af959c92593c4ccece8602ec11bc310d6708
Danny Haslund <dannyhasl1@gmail.com>
2020-02-11 10:47:25 +0100

    Quick fix

--- www/sites/templates/header.php
+++ www/sites/templates/header.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" type="text/css" href="http://localhost:8001/sites/templates/header.css">
+        <link rel="stylesheet" type="text/css" href="/sites/templates/header.css">
     
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
