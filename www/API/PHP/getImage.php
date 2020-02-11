9714f8ac9044cf2e02e2e436d8b00c674df4253c
Nicklas Pihl <sejenick@live.dk>
2020-02-11 12:45:46 +0100

    did something in getimage

--- www/API/PHP/getImage.php
+++ www/API/PHP/getImage.php
@@ -8,6 +8,13 @@
         // output data of each row
         while($row = $result->fetch_assoc()) {
             echo "title: " . $row["title"]. " - description: " . $row["description"]. " " . $row["file"]. "<br>";
+
+            $dirname = $row["file"];
+            $images = glob($dirname."*.png");
+
+            foreach($images as $image) {
+                echo '<img src="'.$image.'" /><br />';
+            }
         }
     } else {
         echo "0 results";
