08b622ba8256b95213131d9551198a34d4a65ace
Danny Haslund <dannyhasl1@gmail.com>
2020-02-13 13:23:38 +0100

    upload styling

--- www/sites/upload/js/main.js
+++ www/sites/upload/js/main.js
@@ -29,6 +29,33 @@ $( document ).ready(function() {
 
             }
         });
+
     });
+
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext("2d");
+    var img = new Image();
+
+    $('#fileToUpload').on('change', (evt) => {
+
+    
+        var files = evt.target.files; // FileList object
+        var file = files[0];
+        if(file.type.match('image.*')) {
+            var reader = new FileReader();
+            // Read in the image file as a data URL.
+            reader.readAsDataURL(file);
+            reader.onload = function(evt){
+                if( evt.target.readyState == FileReader.DONE) {
+                    img.src = evt.target.result;
+                context.drawImage(img,100,100);
+            }
+            }    
+
+        } else {
+            alert("not an image");
+        }
+    });
+    
 
 });
