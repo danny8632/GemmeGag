d8a2643a558ae3cb6979d19a189cab7578b89f47
Danny Haslund <dannyhasl1@gmail.com>
2020-02-13 14:31:36 +0100

    upload comment

--- www/sites/post/js/post.js
+++ www/sites/post/js/post.js
@@ -2,6 +2,10 @@ $( document ).ready(function() {
 
     var post_id = $('.wrapper').data('post-id')
 
+    var html = "";
+
+    var comment_html = "";
+
     data = {
         "post_id": post_id
     };
@@ -22,134 +26,202 @@ $( document ).ready(function() {
 
         return roundedDown;
     }
-    
-    $.ajax({
-        type: "GET",
-        url: "/api_v1/post",
-        data: data,
-        timeout: 600000,
-        success: function (post_data) {
 
-            var post = JSON.parse(post_data)[0]
 
-            console.log(post)
-            var timeSincePost = getHoursSince(post.created);
+    function run() {
+        getPost(() => {
 
-            var html = `
-                <div class="postCon">
-                    <div class="titleCon">
-                        <div class="titleTextCon">
-                            <a class="titleText" href="/post?id=${post.id}">${post.title}</p>
-                        </div>
+            $(".wrapper").html('').append(html);
+
+            getComment(() => {
+
+                $('.commentButtonCon').find('.commentButton').on('click', (e) => {
+                    $('#comments').toggle();
+                })
+
+
+                var commentForm = $('#comments').find('.newCommentForm');
+
+                commentForm.on('submit', (e) => {
+
+                    e.preventDefault();
+
+                    var data = commentForm.serializeArray()[0];
+
+                    if(data.value == "")
+                        return true;
+
+                    var data = {
+                        'post_id' : post_id,
+                        'text' : data.value
+                    }
+
+                    $.ajax({
+                        type: "POST",
+                        url: "/api_v1/comment",
+                        data: data,
+                        timeout: 600000,
+                        success: function (resp_data) {
+
+                            //console.log(resp_data)
+                            getComment(() => {
+                                $('#comments').show();
+                            });
+                        
+                        },
+                        error: function (e) {
+
+                            console.log("ERROR : ", e);
+                
+                        }
+                    });
+                    console.log(data)
+
+                })
+
+            })
+
+        })
+    }
+
+
+    function getPost(callback) {
+        
+        $.ajax({
+            type: "GET",
+            url: "/api_v1/post",
+            data: data,
+            timeout: 600000,
+            success: function (post_data) {
     
-                        <div class="opCon">
-                            <p class="opText">Posted by: <a href="/user?id=${post.userID}">${post.username}</a></p>
-                            <p class="timePosted">${timeSincePost} hour(s) ago</p>
-                        </div>
-                    </div>
+                var post = JSON.parse(post_data)[0]
     
-                    <div class="imgCon">
-                        <img class="postImage" src="${post.file}">
-                    </div>
+                console.log(post)
+                var timeSincePost = getHoursSince(post.created);
     
-                    <div class="descriptionCon">
-                        <p class="description">${post.description}</p>
-                    </div>
-    
-                    <div class="toolbarCon">
-                        <div class="voteCon">
-                            <div class="voteCon2">
-                                <div class="upvote">▲</div>
-                                <div class="totalVotes">${post.totalvotes == null ? '0' : post.totalvotes}</div>
-                                <div class="downvote">▼</div>
+                html = `
+                    <div class="postCon">
+                        <div class="titleCon">
+                            <div class="titleTextCon">
+                                <a class="titleText" href="/post?id=${post.id}">${post.title}</p>
+                            </div>
+        
+                            <div class="opCon">
+                                <p class="opText">Posted by: <a href="/user?id=${post.userID}">${post.username}</a></p>
+                                <p class="timePosted">${timeSincePost} hour(s) ago</p>
                             </div>
                         </div>
-    
-                        <div class="commentButtonCon">
-                            <a class="commentButton">Comments</a>
+        
+                        <div class="imgCon">
+                            <img class="postImage" src="${post.file}">
                         </div>
-    
-                        <div class="shareButtonCon">
-                            <a class="shareButton" onclick="">Share</a>
+        
+                        <div class="descriptionCon">
+                            <p class="description">${post.description}</p>
+                        </div>
+        
+                        <div class="toolbarCon">
+                            <div class="voteCon">
+                                <div class="voteCon2">
+                                    <div class="upvote">▲</div>
+                                    <div class="totalVotes">${post.totalvotes == null ? '0' : post.totalvotes}</div>
+                                    <div class="downvote">▼</div>
+                                </div>
+                            </div>
+        
+                            <div class="commentButtonCon">
+                                <a class="commentButton">Comments</a>
+                            </div>
+        
+                            <div class="shareButtonCon">
+                                <a class="shareButton" onclick="">Share</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            `;
-
-
-            $.ajax({
-                type: "GET",
-                url: "/api_v1/comment",
-                data: data,
-                timeout: 600000,
-                success: function (data) {
-                    var comments = JSON.parse(data)
-
-                    var comment_html = `<div id="comments" class="commentsCon">
-                        <div class="newCommentCon">
-                            <form class="newCommentForm">
-                                <div class="newCommentHead">
-                                    <p class="newCommentHeadText">Post a comment:</p>
-                                </div>
-                                <div class="newCommentTextCon">
-                                    <textarea class="newCommentText" name="commentText" type="text" placeholder="Comment..."></textarea>
-                                </div>
-                                <div class="newCommentButton">
-                                    <input type="submit" value="Post Comment">
-                                </div>
-                            </form>
-                        </div>
-                        `;
-                    
-                    
-                    comments.forEach(comment => {
-                        var timeSincePost = getHoursSince(comment.created);
-                        comment_html += `
-                            <div class="comment">
-                                <div class="commentHead">
-                                    <a class="commentHeadText" href="#">${comment.username}</a>
-                                    <div class="commentTimePosted">${timeSincePost}h</div>
-                                </div>
-                                <div class="commentBody">
-                                    <p class="commentText">${comment.text}</p>
-                                </div>
-                                <div class="commentFoot">
-                                    <div class="commentVotes">
-                                        <div class="commentUpvote">▲</div>
-                                        <div class="commentTotalVotes">${comment.TotalVotes == null ? '0' : comment.TotalVotes}</div>
-                                        <div class="commentDownvote">▼</div>
-                                    </div>
-                                </div>
-                            </div>`
-                    });
-                    
-
-
-                    comment_html += `</div>`
-
-                    html += comment_html
-
-                    $(".wrapper").append(html);
-
-
-
-                    $('.commentButtonCon').find('.commentButton').on('click', (e) => {
-                        $('#comments').toggle();
-                    })
-                },
-                error: function (e) {
-
-                    console.log("ERROR : ", e);
-        
-                }
-            });
+                `;
                 
-            
-        },
-        error: function (e) {
+                callback();
+                
+            },
+            error: function (e) {
+    
+                console.log("ERROR : ", e);
+    
+            }
+        });
+    }
+    
 
-            console.log("ERROR : ", e);
 
-        }
-    });
+    function getComment(callback) {
+    
+        $.ajax({
+            type: "GET",
+            url: "/api_v1/comment",
+            data: data,
+            timeout: 600000,
+            success: function (data) {
+                var comments = JSON.parse(data)
+
+                comment_html = `<div id="comments" class="commentsCon">
+                    <div class="newCommentCon">
+                        <form method="POST" enctype="multipart/form-data" class="newCommentForm">
+                            <div class="newCommentHead">
+                                <p class="newCommentHeadText">Post a comment:</p>
+                            </div>
+                            <div class="newCommentTextCon">
+                                <textarea class="newCommentText" name="commentText" type="text" placeholder="Comment..."></textarea>
+                            </div>
+                            <div class="newCommentButton">
+                                <input type="submit" value="Post Comment">
+                            </div>
+                        </form>
+                    </div>
+                    `;
+                
+                
+                comments.forEach(comment => {
+                    var timeSincePost = getHoursSince(comment.created);
+                    comment_html += `
+                        <div class="comment">
+                            <div class="commentHead">
+                                <a class="commentHeadText" href="#">${comment.username}</a>
+                                <div class="commentTimePosted">${timeSincePost}h</div>
+                            </div>
+                            <div class="commentBody">
+                                <p class="commentText">${comment.text}</p>
+                            </div>
+                            <div class="commentFoot">
+                                <div class="commentVotes">
+                                    <div class="commentUpvote">▲</div>
+                                    <div class="commentTotalVotes">${comment.TotalVotes == null ? '0' : comment.TotalVotes}</div>
+                                    <div class="commentDownvote">▼</div>
+                                </div>
+                            </div>
+                        </div>`
+                });
+                
+
+
+                comment_html += `</div>`
+
+                $("#comments").remove();
+
+                $('.wrapper').append(comment_html)
+
+                
+                callback();
+
+
+            },
+            error: function (e) {
+
+                console.log("ERROR : ", e);
+    
+            }
+        });
+    }
+
+
+    run();
 });
