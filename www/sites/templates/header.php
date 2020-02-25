108acd91993663015bef16b9d0896b036ae44e54
Danny Haslund <dannyhasl1@gmail.com>
2020-02-25 12:27:14 +0100

    navbar WIP

--- www/sites/templates/header.php
+++ www/sites/templates/header.php
@@ -43,20 +43,22 @@
     <body>
 
     <div class="topnav" id="myTopnav">
-        <a href="#home">Home</a>
-        <a href="#news" class="active">News</a>
-        <a href="#contact">Contact</a>
-        <div class="dropdown">
-            <button class="dropbtn">Dropdown 
-            <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
-            </div>
-        </div> 
-        <a href="#about">About</a>
+        <div class="nav-btn-wrapper">
+            <a href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <div class="dropdown">
+                <button class="dropbtn">Dropdown 
+                <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+                </div>
+            </div> 
+            <a href="#about">About</a>
+        </div>
         <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
     </div>
 
--- www/sites/templates/js/navbar.js
+++ www/sites/templates/js/navbar.js
@@ -1,8 +1,21 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "topnav";
+        x.className = "topnav";
     }
 }
+
+$(document).ready(function () {
+    
+
+    $("#myTopnav").find('.dropbtn').on('click', (e) => {
+
+        console.log($(e.target)[0])
+
+        $(e.target).parents('.dropdown').children('.dropdown-content').toggleClass("shownen");
+    })
+
+
+});
