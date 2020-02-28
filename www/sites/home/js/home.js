67a75d05c50f5898c78d611581af545fff3dc4f7
ben <benjaminsandland@gmail.com>
2020-02-28 08:36:13 +0100

    Fixed top/trending/new - no longer changes page

--- www/sites/home/js/home.js
+++ www/sites/home/js/home.js
@@ -1,94 +1,284 @@
 $(document).ready(function () {
 
-    $.ajax({
-        type: "GET",
-        url: "/api_v1/post",
-        timeout: 600000,
-        success: function (data) {
+    function bindEventHandlers() {
+        $('.content').find('.newButtonCon').on('click', (e) => {
+            $('.content').find('#posts').empty();
+            getPosts();
+            changeHeadlineTxt('New posts');
+        });
 
-            var response = JSON.parse(data)
+        $('.content').find('.trendingButtonCon').on('click', (e) => {
+            $('.content').find('#posts').empty();
+            getTrending();
+            changeHeadlineTxt('Trending posts');
+        });
 
-            console.log(response)
+        $('.content').find('.topButtonCon').on('click', (e) => {
+            $('.content').find('#posts').empty();
+            getTop();
+            changeHeadlineTxt('Top posts');
+        });
 
-            for (var i = 0; i < response.length; ++i) {
-                var post = response[i];
-                var timeSincePost = getTimeSince(post.created);
 
-                var fileHtml = getPostType(post.file);
-                var imgConId = `postMedia${i + 1}`
 
-                var html = `
-                    <div class="postCon">
-                        <div class="titleCon">
-                            <div class="titleTextCon">
-                                <a class="titleText" href="/post?id=${post.id}">${post.title}</p>
-                            </div>
-        
-                            <div class="opCon">
-                                <p class="opText">Posted by: <a href="/user?id=${post.userID}">${post.username}</a></p>
-                                <p class="timePosted">${timeSincePost}</p>
-                            </div>
-                        </div>
-        
-                        <div class="imgCon" id="${imgConId}">
-                            
-                        </div>
-        
-                        <div class="descriptionCon">
-                            <p class="description">${post.description}</p>
-                        </div>
-        
-                        <div class="toolbarCon">
-                            <div class="voteCon">   
-                                <div class="voteCon2">
-                                <div class="upvote votebtn ${(post.your_vote != null && post.your_vote == "Upvote") ? "upduttet" : ''}" data-post_id="${post.id}" data-vote="Upvote">▲</div>
-                                <div class="totalVotes">${post.TotalVotes == null ? '0' : post.TotalVotes}</div>
-                                <div class="downvote votebtn ${(post.your_vote != null && post.your_vote == "Downvote") ? "downduttet" : ''}" data-post_id="${post.id}" data-vote="Downvote">▼</div>
+        $('#posts').find('.votebtn').on('click', (e) => {
+
+            vote($(e.target).data(), (req_data) => {
+
+                $(e.target).parent().children(":not(.votebtn)").html(req_data[0].TotalVotes == null ? '0' : req_data[0].TotalVotes)
+
+                $(e.target).removeClass('upduttet downduttet').siblings('.votebtn').removeClass('upduttet downduttet');
+
+                if (req_data[0].your_vote == "Downvote") {
+                    $(e.target).parent().children(".downvote").toggleClass("downduttet", true)
+                }
+                else if (req_data[0].your_vote == "Upvote") {
+                    $(e.target).parent().children(".upvote").toggleClass("upduttet", true)
+                }
+            });
+        });
+    }
+
+    function changeHeadlineTxt(text) {
+        $('.content').find('.headlineTxt').text(`${text}`);
+    }
+
+    function getPosts() {
+        $.ajax({
+            type: "GET",
+            url: "/api_v1/post",
+            timeout: 600000,
+            success: function (data) {
+
+                var response = JSON.parse(data)
+
+                console.log(response)
+
+                for (var i = 0; i < response.length; ++i) {
+                    var post = response[i];
+                    var timeSincePost = getTimeSince(post.created);
+
+                    var fileHtml = getPostType(post.file);
+                    var imgConId = `postMedia${i + 1}`
+
+                    var html = `
+                        <div class="postCon">
+                            <div class="titleCon">
+                                <div class="titleTextCon">
+                                    <a class="titleText" href="/post?id=${post.id}">${post.title}</p>
+                                </div>
+            
+                                <div class="opCon">
+                                    <p class="opText">Posted by: <a href="/user?id=${post.userID}">${post.username}</a></p>
+                                    <p class="timePosted">${timeSincePost}</p>
                                 </div>
                             </div>
-        
-                            <div class="commentButtonCon">
-                                <a class="commentButton" href="/post?id=${post.id}">Comments</a>
+            
+                            <div class="imgCon" id="${imgConId}">
+                                
                             </div>
-        
-                            <div class="shareButtonCon">
-                                <a class="shareButton" href="/post?id=${post.id}">Share</a>
+            
+                            <div class="descriptionCon">
+                                <p class="description">${post.description}</p>
+                            </div>
+            
+                            <div class="toolbarCon">
+                                <div class="voteCon">   
+                                    <div class="voteCon2">
+                                    <div class="upvote votebtn ${(post.your_vote != null && post.your_vote == "Upvote") ? "upduttet" : ''}" data-post_id="${post.id}" data-vote="Upvote">▲</div>
+                                    <div class="totalVotes">${post.TotalVotes == null ? '0' : post.TotalVotes}</div>
+                                    <div class="downvote votebtn ${(post.your_vote != null && post.your_vote == "Downvote") ? "downduttet" : ''}" data-post_id="${post.id}" data-vote="Downvote">▼</div>
+                                    </div>
+                                </div>
+            
+                                <div class="commentButtonCon">
+                                    <a class="commentButton" href="/post?id=${post.id}">Comments</a>
+                                </div>
+            
+                                <div class="shareButtonCon">
+                                    <a class="shareButton" href="/post?id=${post.id}">Share</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                `
+                    `
 
-                $("#posts").append(html);
+                    $("#posts").append(html);
 
-                $(".imgCon#" + imgConId).append(fileHtml);
+                    $(".imgCon#" + imgConId).append(fileHtml);
+                }
+
+                //$('#posts').find('.votebtn').on('click', (e) => vote($(e.target).data()))
+
+            },
+            error: function (e) {
+
+                console.log("ERROR : ", e);
+
             }
+        });
 
-            $('#posts').find('.votebtn').on('click', (e) => {
+    }
 
-                vote($(e.target).data(), (req_data) => {
+    function getTop() {
+        data = {
+            "method": "getTop"
+        };
 
-                    $(e.target).parent().children(":not(.votebtn)").html(req_data[0].TotalVotes == null ? '0' : req_data[0].TotalVotes)
+        $.ajax({
+            type: "GET",
+            url: "/api_v1/post",
+            data: data,
+            timeout: 600000,
+            success: function (data) {
+                console.log(data)
+                var response = JSON.parse(data)
 
-                    $(e.target).removeClass('upduttet downduttet').siblings('.votebtn').removeClass('upduttet downduttet');
+                console.log(response)
 
-                    if (req_data[0].your_vote == "Downvote") {
-                        $(e.target).parent().children(".downvote").toggleClass("downduttet", true)
-                    }
-                    else if (req_data[0].your_vote == "Upvote") {
-                        $(e.target).parent().children(".upvote").toggleClass("upduttet", true)
-                    }
-                });
-            });
+                for (var i = 0; i < response.length; ++i) {
+                    var post = response[i];
+                    var timeSincePost = getTimeSince(post.created);
 
+                    var fileHtml = getPostType(post.file);
+                    var imgConId = `postMedia${i + 1}`
 
-            //$('#posts').find('.votebtn').on('click', (e) => vote($(e.target).data()))
+                    var html = `
+                         <div class="postCon">
+                             <div class="titleCon">
+                                 <div class="titleTextCon">
+                                     <a class="titleText" href="/post?id=${post.id}">${post.title}</p>
+                                 </div>
+             
+                                 <div class="opCon">
+                                     <p class="opText">Posted by: <a href="/user?id=${post.userID}">${post.username}</a></p>
+                                     <p class="timePosted">${timeSincePost}</p>
+                                 </div>
+                             </div>
+             
+                             <div class="imgCon" id="${imgConId}">
+                                 
+                             </div>
+             
+                             <div class="descriptionCon">
+                                 <p class="description">${post.description}</p>
+                             </div>
+             
+                             <div class="toolbarCon">
+                                 <div class="voteCon">   
+                                     <div class="voteCon2">
+                                         <div class="upvote votebtn" data-post_id="${post.id}" data-vote="Upvote">▲</div>
+                                         <div class="totalVotes">${post.TotalVotes == null ? '0' : post.TotalVotes}</div>
+                                         <div class="downvote votebtn" data-post_id="${post.id}" data-vote="Downvote">▼</div>
+                                     </div>
+                                 </div>
+             
+                                 <div class="commentButtonCon">
+                                     <a class="commentButton" href="/post?id=${post.id}">Comments</a>
+                                 </div>
+             
+                                 <div class="shareButtonCon">
+                                     <a class="shareButton" href="/post?id=${post.id}">Share</a>
+                                 </div>
+                             </div>
+                         </div>
+                     `
 
-        },
-        error: function (e) {
+                    $("#posts").append(html);
 
-            console.log("ERROR : ", e);
+                    $(".imgCon#" + imgConId).append(fileHtml);
+                }
 
-        }
-    });
+                //$('#posts').find('.votebtn').on('click', (e) => vote($(e.target).data()))
+
+            },
+            error: function (e) {
+
+                console.log("ERROR : ", e);
+
+            }
+        });
+    }
+
+    function getTrending() {
+        data = {
+            "method": "getTrending"
+        };
+
+        $.ajax({
+            type: "GET",
+            url: "/api_v1/post",
+            data: data,
+            timeout: 600000,
+            success: function (data) {
+                console.log(data)
+                var response = JSON.parse(data)
+
+                console.log(response)
+
+                for (var i = 0; i < response.length; ++i) {
+                    var post = response[i];
+                    var timeSincePost = getTimeSince(post.created);
+
+                    var fileHtml = getPostType(post.file);
+                    var imgConId = `postMedia${i + 1}`
+
+                    var html = `
+                        <div class="postCon">
+                            <div class="titleCon">
+                                <div class="titleTextCon">
+                                    <a class="titleText" href="/post?id=${post.id}">${post.title}</p>
+                                </div>
+            
+                                <div class="opCon">
+                                    <p class="opText">Posted by: <a href="/user?id=${post.userID}">${post.username}</a></p>
+                                    <p class="timePosted">${timeSincePost}</p>
+                                </div>
+                            </div>
+            
+                            <div class="imgCon" id="${imgConId}">
+                                
+                            </div>
+            
+                            <div class="descriptionCon">
+                                <p class="description">${post.description}</p>
+                            </div>
+            
+                            <div class="toolbarCon">
+                                <div class="voteCon">   
+                                    <div class="voteCon2">
+                                        <div class="upvote votebtn" data-post_id="${post.id}" data-vote="Upvote">▲</div>
+                                        <div class="totalVotes">${post.TotalVotes == null ? '0' : post.TotalVotes}</div>
+                                        <div class="downvote votebtn" data-post_id="${post.id}" data-vote="Downvote">▼</div>
+                                    </div>
+                                </div>
+            
+                                <div class="commentButtonCon">
+                                    <a class="commentButton" href="/post?id=${post.id}">Comments</a>
+                                </div>
+            
+                                <div class="shareButtonCon">
+                                    <a class="shareButton" href="/post?id=${post.id}">Share</a>
+                                </div>
+                            </div>
+                        </div>
+                    `
+
+                    $("#posts").append(html);
+
+                    $(".imgCon#" + imgConId).append(fileHtml);
+                }
+
+                //$('#posts').find('.votebtn').on('click', (e) => vote($(e.target).data()))
+
+            },
+            error: function (e) {
+
+                console.log("ERROR : ", e);
+
+            }
+        });
+    }
+
+    bindEventHandlers();
 
 });
--- www/sites/upload/js/main.js
+++ www/sites/upload/js/main.js
@@ -1,13 +1,13 @@
-$( document ).ready(function() {
-    
+$(document).ready(function () {
+
     var form = $(`form.uploadPost`);
 
-    form.on( "submit", function( event ) {
+    form.on("submit", function (event) {
         event.preventDefault();
 
         var form = $(`form.uploadPost`)[0];
 
-		// Create an FormData object 
+        // Create an FormData object 
         var data = new FormData(form);
 
         $.ajax({
@@ -20,7 +20,7 @@ $( document ).ready(function() {
             cache: false,
             timeout: 600000,
             success: function (data) {
-
+                console.log(data);
                 data = JSON.parse(data);
 
                 console.log("SUCCESS : ", data);
@@ -43,57 +43,57 @@ $( document ).ready(function() {
 
     });
 
-    
+
 
     $('#fileToUpload').on('change', (evt) => {
         console.log("event")
 
         $.extend(new FileReader(), {
-            onload: function(e) {
+            onload: function (e) {
                 $("#img").attr("src", URL.createObjectURL(new Blob([e.target.result]))).css("display", "block")
             }
         }).readAsArrayBuffer(evt.target.files[0])
     });
 
 
-    $("[type=file]").change(function() {
+    $("[type=file]").change(function () {
 
         $.extend(new FileReader(), {
-            onload: function(e) {
+            onload: function (e) {
                 img.attr("src", URL.createObjectURL(new Blob([e.target.result])))
             }
         }).readAsArrayBuffer(this.files[0])
     })
-    
+
     // Get the modal
     var modal = document.getElementById("myModal");
 
     // Get the button that opens the modal
     var btn = document.getElementById("openModal");
-    
+
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-            var title = document.forms["uploadPost"]["title"].value;
-            var desc = document.forms["uploadPost"]["description"].value;
-            var img = document.forms["uploadPost"]["fileToUpload"].value;
-            if (title == "" || desc == "" || img == "") {
-                
-            }
-            else {
-                modal.style.display = "block"
-            }
+    btn.onclick = function () {
+        var title = document.forms["uploadPost"]["title"].value;
+        var desc = document.forms["uploadPost"]["description"].value;
+        var img = document.forms["uploadPost"]["fileToUpload"].value;
+        if (title == "" || desc == "" || img == "") {
+
+        }
+        else {
+            modal.style.display = "block"
+        }
     }
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+    span.onclick = function () {
         modal.style.display = "none";
     }
 
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
